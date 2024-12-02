c09f25cc37dc68714af4873b1d517dc039bee4f2
Adrian Isasi <aisasi505@gmail.com>
2024-12-02 17:55:22 +0100

    pequeños cambios antes de entregar

--- Assets/Scripts/DontDestroyScript.cs
+++ Assets/Scripts/DontDestroyScript.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DontDestroyScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        DontDestroyOnLoad(gameObject);   
+    {  
+        if(FindObjectsByType<DontDestroyScript>(FindObjectsSortMode.None).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
     }
 }
